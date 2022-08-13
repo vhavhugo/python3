0ef5568e496fc5fc45f26d458c6e64a5da1e1a37
vhavhugo <vhavhugo@hotmail.com>
2022-08-13 01:14:14 -0300

    Aula 5 - Operadores aritimetricos

---
+++ aula5.py
@@ -0,0 +1,17 @@
+"""
++, -, *, /, //, **, %, (), +-, */
+"""
+print(5+5)  #10
+print('5'+5)  #55
+
+print(5 * 5)  #25
+print(5 * '5') #55555  Repetição
+
+print(10.5 // 3)  # divisão inteira = arredonda o valor
+
+print(2 ** 2)  #Potenciação
+
+print(5 % 2)  #Resto da divisão
+
+print(2+5*10)  #52
+print((2+5)*10)  #70
